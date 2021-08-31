4b8b8cdfb5ff4fd9c3e6f1d07858e96db40a47ee
zalba monteiro <rosalbasmonteiro@gmail.com>
2021-08-31 14:39:13 -0300

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const consign = require('consign');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
